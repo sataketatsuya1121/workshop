7b58c946c4d37a990fd384b1b765b8a5e7aba65e
佐竹達也 <sataketatsuya@tatsuya-mbp.local>
2019-12-03 16:47:37 +0900

    first commit

---
+++ www/html/config.php
@@ -0,0 +1,16 @@
+<?php
+    define('DSN', 'mysql:host=localhost;dbname=mydb;charset=utf8');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', 'secret');
+
+    function connectPdo()
+    {
+        try {
+            return new PDO(DSN, DB_USER, DB_PASSWORD);
+        }
+        catch (PDOException $e){
+            echo $e->getMessage();
+            exit();
+        }
+    }
+?>
