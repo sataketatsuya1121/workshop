57bbcd672903e7d9c7c8ee9c2375e788ce2a8e87
佐竹達也 <sataketatsuya@tatsuya-mbp.local>
2019-12-04 18:35:26 +0900

    Docker環境を構築しました。

--- www/html/config.php
+++ www/html/config.php
@@ -1,5 +1,5 @@
 <?php
-    define('DSN', 'mysql:host=localhost;dbname=mydb;charset=utf8');
+    define('DSN', 'mysql:host=172.29.0.2;dbname=mydb;charset=utf8');
     define('DB_USER', 'root');
     define('DB_PASSWORD', 'secret');
 
